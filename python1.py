8dccc0bbb9847e850169b29a53ed2552942f68de
manuelnuma7 <xnuma777x@gmail.com>
2022-11-03 17:42:04 +0100

    Cambios

---
+++ python1.py
@@ -0,0 +1,8 @@
+nombre=input("¿Cómo te llamas? ")
+if nombre == "":
+    print("No has escrito nada",end=" ")
+else:
+    print("Hola "+ nombre + " encantado",end=" ")
+print("Adiós")
+    
+    
---
+++ python1b.py
@@ -0,0 +1,18 @@
+"""
+comentario
+de
+varias
+líneas
+"""
+
+
+while True:
+    nombre=input("¿Cómo te llamas? ")
+    if nombre == "":
+        print("No has escrito nada",end=" ")
+    else:
+        print(f"Hola {nombre} encantado",end=" ")
+        print("Adiós")
+        break;
+    
+    
